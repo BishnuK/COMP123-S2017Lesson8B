1721a68e4b34d0c1fbeb921d5829786edfad0435
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-07-04 13:26:22 -0400

    Added Person class

--- COMP123-S2017Lesson8B/Person.cs
+++ COMP123-S2017Lesson8B/Person.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace COMP123_S2017Lesson8B
 {
+    
+    /// <summary>
+    /// This is the person class
+    /// </summary>
     class Person
     {
+        // private instance variables(fields)
+        private string _name;
+        private int _age;
+        
+        //public properties................
+         public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this._age;
+            }
+            set
+            {
+                this._age = value;
+            }
+        }
+        //constructors...................
+        /// <summary>
+        /// This is the constructor for the person class.
+        /// It takes two arguments-name (string) and age(int)
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="age"></param>
+        public Person(string name, int age)
+        {
+            //constructor gets triggered when we create (instantiate) object
+            this.Name = _name;
+            this.Age = _age;
+        }
+        //public methods................methods are behaviours..
+        /// <summary>
+        /// This method enables the Person object to talk 
+        /// </summary>
+        public void Talks()
+        {
+            Console.WriteLine(this.Name+ "is talking");
+        }
     }
 }
--- COMP123-S2017Lesson8B/Program.cs
+++ COMP123-S2017Lesson8B/Program.cs
@@ -10,6 +10,14 @@ namespace COMP123_S2017Lesson8B
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n");
+            Person person = new Person("Tom", 25);
+            person.Talks();
+            
+            //person is a new object, . dot is used to access the 'Talks' method
+
+            Console.WriteLine("\n");
+
         }
     }
 }
